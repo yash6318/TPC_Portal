d3615e765e9d2c40ecb649fb44eb5134f64c1332
Parth-2709 <parth.kesarwani.cse21@itbhu.ac.in>
2023-10-21 21:18:32 +0530

    change password

--- dbms-project-main/spring-boot-example/src/main/java/com/dbms/project/dao/UserDao.java
+++ dbms-project-main/spring-boot-example/src/main/java/com/dbms/project/dao/UserDao.java
@@ -66,7 +66,7 @@ public class UserDao {
         jdbcTemplate.update(updateSql,
                 user.getPassword(),
                 user.getUsername()
-        );
-    }
+        );
+    }
     
 }
