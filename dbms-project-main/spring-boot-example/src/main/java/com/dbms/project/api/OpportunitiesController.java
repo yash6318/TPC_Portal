a5bab9667b0144e384dbcb21172282fcb1f5ec15
Kushagra Gupta <kgkushagrakg8@gmail.com>
2023-10-25 03:42:55 +0530

    created roles.html for company Willingness now can be added using change willingness

--- dbms-project-main/spring-boot-example/src/main/java/com/dbms/project/api/OpportunitiesController.java
+++ dbms-project-main/spring-boot-example/src/main/java/com/dbms/project/api/OpportunitiesController.java
@@ -1,21 +1,19 @@
 package com.dbms.project.api;
 
-import com.dbms.project.model.Role;
-import com.dbms.project.model.Student;
-import com.dbms.project.model.User;
-import com.dbms.project.service.BranchService;
-import com.dbms.project.service.CompanyService;
-import com.dbms.project.service.RoleService;
-import com.dbms.project.service.StudentService;
+import com.dbms.project.model.*;
+import com.dbms.project.service.*;
 import com.zaxxer.hikari.util.IsolationLevel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class OpportunitiesController {
@@ -29,6 +27,10 @@ public class OpportunitiesController {
     RoleService roleService;
     @Autowired
     BranchService branchService;
+    @Autowired
+    ResumeService resumeService;
+    @Autowired
+    WillingnessService willingnessService;
 
     @GetMapping("/opportunities")
     public String opportunities(Authentication auth, Model model){
@@ -41,6 +43,7 @@ public class OpportunitiesController {
         List<Role> roles = roleService.getRoles(student);
         List<List<String>> branches = new ArrayList<>();
         List<String> company = new ArrayList<>();
+        List<Resume> resumes = resumeService.getResumesByUser(Integer.parseInt(user.getUsername()));
         for(Role role: roles){
             branches.add(branchService.getBranchFromBin(role.getBranchValue()));
             company.add(companyService.getCompanyByID(role.getCompanyID()).getCompanyName());
@@ -48,8 +51,22 @@ public class OpportunitiesController {
         model.addAttribute("roles", roles);
         model.addAttribute("branches", branches);
         model.addAttribute("company", company);
+        model.addAttribute("resumes", resumes);
         return "opportunities";
     }
+
+    @PostMapping("/opportunities")
+    public String saveOpportunities(@ModelAttribute Willingness willingness, Authentication auth){
+        User user = (User)auth.getPrincipal();
+        if(!Objects.equals(willingness.getResumeName(), "NO")){
+            willingness.setRollNo(Integer.parseInt(user.getUsername()));
+            System.out.println(willingness);
+            willingnessService.insertWillingness(willingness);
+        }
+        // else me delete bhi karna padega;
+        return "redirect:/opportunities";
+    }
+
 
 
 }
